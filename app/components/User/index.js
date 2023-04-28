49a244bf6717a6d2d9d7765f823fc133d9f61378
ellipsecode007 <ishupriyam@gmail.com>
2023-04-27 23:04:55 -0700

    Share only enable when not shared

--- app/components/User/index.js
+++ app/components/User/index.js
@@ -1,166 +1,165 @@
-"use client"
-import React, { useState, useEffect } from "react";
-import Game from "../Game";
-import db from "../firebase";
-import queryString from "query-string";
-import { useRouter } from "next/navigation";
-import { useLocation } from 'react-router-dom';
-import '../User/index.css';
+    "use client"
+    import React, { useState, useEffect } from "react";
+    import Game from "../Game";
+    import db from "../firebase";
+    import queryString from "query-string";
+    import { useRouter } from "next/navigation";
+    import { useLocation } from 'react-router-dom';
+    import '../User/index.css';
 
 
-function User() {
-    const [username, setUsername] = useState("");
-    const [renderGame, setRenderGame] = useState(false);
-    const [renderBoard, setRenderBoard] = useState(false);
-    const [scores, setScores] = useState([]);
-    const [customWord, setCustomWord] = useState("");
-    const [link, setLink] = useState("");
-    const queryParameters = new URLSearchParams(window.location.search);
-    const word = queryParameters.get("word");
-    console.log(word);  
-    // const query = new URLSearchParams(location.search);
-    // const word = query.get('word');
-    // console.log(router);
-    //  const {word} = router.query;
-    function handleInputChange(event) {
-        setUsername(event.target.value);
-    }
-    function handleInputChange2(event) {
-        setCustomWord(event.target.value);
-    }
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        if(!username){
-            alert('Please Enter username');
+    function User() {
+        const [username, setUsername] = useState("");
+        const [renderGame, setRenderGame] = useState(false);
+        const [renderBoard, setRenderBoard] = useState(false);
+        const [scores, setScores] = useState([]);
+        const [customWord, setCustomWord] = useState("");
+        const [link, setLink] = useState("");
+        const queryParameters = new URLSearchParams(window.location.search);
+        const word = queryParameters.get("word");
+        console.log(word); 
+        // const query = new URLSearchParams(location.search);
+        // const word = query.get('word');
+        // console.log(router);
+        //  const {word} = router.query;
+        function handleInputChange(event) {
+            setUsername(event.target.value);
         }
-        else{
-            // console.log(word);
-            if(word)
-            {
-             setCustomWord(atob(word));
+        function handleInputChange2(event) {
+            setCustomWord(event.target.value);
+        }
+
+        function handleSubmit(event) {
+            event.preventDefault();
+            if(!username){
+                alert('Please Enter username');
             }
-            setRenderGame(true);
+            else{
+                // console.log(word);
+                if(word)
+                {
+                setCustomWord(atob(word));
+                }
+                setRenderGame(true);
+            }
         }
-    }
-    function handleSubmit2(event) {
-        event.preventDefault();
-        if(!customWord){
-            alert('Please Enter word');
+        function handleSubmit2(event) {
+            event.preventDefault();
+            if(!customWord){
+                alert('Please Enter word');
+            }
+            else{
+                console.log(customWord);
+                const encodedWord = btoa(customWord);
+                console.log(encodedWord);
+                const queryParams = queryString.stringify({ word: encodedWord });
+                const url = `${window.location.origin}?${queryParams}`;
+                console.log(url);
+                setLink(url);
+            }
         }
-        else{
-            console.log(customWord);
-            const encodedWord = btoa(customWord);
-            console.log(encodedWord);
-            const queryParams = queryString.stringify({ word: encodedWord });
-            const url = `${window.location.origin}?${queryParams}`;
-            console.log(url);
-            setLink(url);
+
+        function handleBackClick() {
+            setUsername("");
+            setRenderGame(false);
+            setRenderBoard(false);
         }
-    }
+        function handleBoardClick(){
+            setRenderBoard(true);
+        }
+        function copyLink() {
+            var linkField = document.getElementById("link-field");
+        
+            linkField.select();
+        
+        
+            document.execCommand("copy");
+        
+        alert("Copied the link: " + linkField.value);
 
-    function handleBackClick() {
-        setUsername("");
-        setRenderGame(false);
-        setRenderBoard(false);
-    }
-    function handleBoardClick(){
-        setRenderBoard(true);
-    }
-    function copyLink() {
-        var linkField = document.getElementById("link-field");
-      
-        linkField.select();
-      
-      
-        document.execCommand("copy");
-      
-       alert("Copied the link: " + linkField.value);
+        }
 
-    }
-
-    useEffect(() => {
-        const unsubscribe = db.collection('scores')
-          .orderBy('sc', 'desc')
-          .limit(10)
-          .onSnapshot((querySnapshot) => {
-            const newScores = [];
-            querySnapshot.forEach((doc) => {
-              const data = doc.data();
-              console.log(data);
-              newScores.push({
-                id: doc.id,
-                username: data.Name,
-                score: data.sc,
-              });
+        useEffect(() => {
+            const unsubscribe = db.collection('scores')
+            .orderBy('sc', 'desc')
+            .limit(10)
+            .onSnapshot((querySnapshot) => {
+                const newScores = [];
+                querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                console.log(data);
+                newScores.push({
+                    id: doc.id,
+                    username: data.Name,
+                    score: data.sc,
+                });
+                });
+                setScores(newScores);
+                console.log(scores);
             });
-            setScores(newScores);
-            console.log(scores);
-          });
-        },[]);
-    return (
-        <div>
-            {!renderGame && !renderBoard? (
-                <>
-                <div>
-                    <h1 className="first">Hangman Team : 26</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label>
-                            Username:
-                            <input type="text" value={username} onChange={handleInputChange} />
-                        </label>
-                        <button className="submit" type="submit">Play</button>
-                    </form>
-                    <form onSubmit={handleSubmit2}>
-                        <label>
-                            Custom Word
-                            <input type="text" value={customWord} onChange={handleInputChange2} />
-                        </label>
-                        <button className="submit" type="submit">Share</button>
-                    </form>
-                    <div class="link-box">
-                        <p>Here is the Link:</p>
-                        <p class="link">{link}</p>
-                    </div>
+            },[]);
+        return (
+            <div>
+                {!renderGame && !renderBoard? (
+                    <>
                     <div>
-                        <button className="submit" onClick={handleBoardClick}>ScoreBoard</button>
+                        <h1 className="first">Hangman Team : 26</h1>
+                        <form onSubmit={handleSubmit}>
+                            <label>
+                                Username:
+                                <input type="text" value={username} onChange={handleInputChange} />
+                            </label>
+                            <button className="submit" type="submit">Play</button>
+                        </form>
+                        {word==null?(
+                        <form onSubmit={handleSubmit2}>
+                            <label>
+                                Custom Word
+                                <input type="text" value={customWord} onChange={handleInputChange2} />
+                            </label>
+                            <button className="submit" type="submit">Share</button>
+                        </form>
+                        ):null}
+                        {link&&(
+                        <div class="link-box">
+                            <p>Here is the Link:</p>
+                            <p class="link">{link}</p>
+                        </div>
+                        )}
+                        <div>
+                            <button className="submit" onClick={handleBoardClick}>ScoreBoard</button>
+                        </div>
                     </div>
-                </div>
-
-                {/* <div className="scoreBoard">
-                    <button className="board">ScoreBoard</button>
-                </div> */}
-                 
-                </>
-            ) : !renderBoard? (
-                <Game username={username} customWord = {customWord}onBackClick={handleBackClick} />
-            ):(
-                <div>
-                    <button className="submit" onClick={handleBackClick}>Back
-                    </button>
-                <h2>Top Scores</h2>
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Rank</th>
-                        <th>Username</th>
-                        <th>Score</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {scores.map((score, index) => (
-                        <tr key={score.id}>
-                        <td>{index + 1}</td>
-                        <td>{score.username}</td>
-                        <td>{score.score}</td>
+                    </>
+                ) : !renderBoard? (
+                    <Game username={username} customWord = {customWord}onBackClick={handleBackClick} />
+                ):(
+                    <div>
+                        <button className="submit" onClick={handleBackClick}>Back
+                        </button>
+                    <h2>Top Scores</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Rank</th>
+                            <th>Username</th>
+                            <th>Score</th>
                         </tr>
-                    ))}
-                    </tbody>
-                </table>
-                </div>
-            )}
-        </div>
-    );
-}
+                        </thead>
+                        <tbody>
+                        {scores.map((score, index) => (
+                            <tr key={score.id}>
+                            <td>{index + 1}</td>
+                            <td>{score.username}</td>
+                            <td>{score.score}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                    </div>
+                )}
+            </div>
+        );
+    }
 
-export default User;
+    export default User;
